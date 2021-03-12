cc911be27c86b9913e068975c00c36d82148a41f
Diego <difemo91@gmail.com>
2021-03-12 15:47:07 -0500

    [FEAT - DART] add rectangle class with one line constructor

---
+++ dart/rectangle.dart
@@ -0,0 +1,21 @@
+// rectangle class
+
+import 'dart:math';
+
+class Rectangle {
+  int width;
+  int height;
+  Point origin;
+
+  Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
+
+  @override
+  String toString() => 'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
+}
+
+void main() {
+  print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 200));
+  print(Rectangle());
+}
